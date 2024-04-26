8ef658bf1b88b156ae28739cda0d49f3eccfd434
eds0c <eduardo.carbajal@pucp.edu.pe>
2024-04-25 19:15:51 -0500

    primer commit

---
+++ src/main/java/com/example/lab4_20180444/Lab420180444Application.java
@@ -0,0 +1,10 @@
+package com.example.lab4_20180444;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab420180444Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab420180444Application.class, args
---
+++ src/test/java/com/example/lab4_20180444/Lab420180444ApplicationTests.java
@@ -0,0 +1,10 @@
+package com.example.lab4_20180444;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab420180444ApplicationTests {
+
+    @Test
+    void contextLoads() {
